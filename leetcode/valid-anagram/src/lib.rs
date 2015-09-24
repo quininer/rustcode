05580cfe40c65a00e74220c81259e95c95f0a35b
quininer <quininer@live.com>
2015-09-24 10:18:21 +0800

    fix valid-anagram

--- leetcode/valid-anagram/src/lib.rs
+++ leetcode/valid-anagram/src/lib.rs
@@ -4,23 +4,16 @@ fn is_anagram(s: &str, t: &str) -> bool {
         return false;
     };
 
-    for b in s.chars() {
-        if t.find(b).is_none() {
-            return false;
-        };
-    };
-
-    for b in t.chars() {
-        if s.find(b).is_none() {
-            return false;
-        };
-    };
-
-    true
+    let mut sv = s.chars().rev().collect::<Vec<char>>();
+    let mut tv = t.chars().rev().collect::<Vec<char>>();
+    sv.sort();
+    tv.sort();
+    sv == tv
 }
 
 #[test]
 fn it_works() {
     assert!(is_anagram("anagram", "nagaram"));
     assert!(!is_anagram("rat", "cat"));
+    assert!(!is_anagram("abb", "baa"));
 }
