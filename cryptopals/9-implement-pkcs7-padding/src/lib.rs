430a11ef4b4c5a3a40620da2cf81fc178f8654a9
quininer <quininer@live.com>
2015-12-12 19:59:50 +0800

    add 9

---
+++ cryptopals/9-implement-pkcs7-padding/src/lib.rs
@@ -0,0 +1,16 @@
+use std::iter;
+
+pub fn pksc7padding(data: &[u8], len: usize) -> Vec<u8> {
+    let pad = len - (data.len() % len);
+    let mut result = data.to_vec();
+    result.append(&mut iter::repeat(pad as u8).take(pad).collect());
+    result
+}
+
+#[test]
+fn it_works() {
+    assert_eq!(
+        pksc7padding(b"YELLOW SUBMARINE", 20),
+        b"YELLOW SUBMARINE\x04\x04\x04\x04"
+    );
+}
