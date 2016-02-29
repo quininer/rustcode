3d77333b36e822549114a5c1204e8a79fb5a4fcf
quininer kel <quininer@live.com>
2016-02-29 19:40:00 +0800

    no_mangle should unsafe

--- dymaster/examples/slave/src/lib.rs
+++ dymaster/examples/slave/src/lib.rs
@@ -1,9 +1,9 @@
 #[no_mangle]
-pub extern fn foo(bar: isize) -> isize {
+pub unsafe fn foo(bar: isize) -> isize {
     bar.pow(bar as u32)
 }
 
 #[test]
 fn it_works() {
-    assert_eq!(foo(4), 256);
+    assert_eq!(unsafe { foo(4) }, 256);
 }
