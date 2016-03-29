97d8faeaa0dba5bf45600ff73fb9e201930fc2ba
quininer kel <quininer@live.com>
2016-03-30 05:15:41 +0800

    add(repo) implement-a-sha-1-keyed-mac

---
+++ cryptopals/28-implement-a-sha-1-keyed-mac/src/lib.rs
@@ -0,0 +1,39 @@
+#![feature(question_mark)]
+
+extern crate rustc_serialize;
+extern crate byteorder;
+
+mod sha1;
+
+pub use sha1::{ Sha1, Digest, padding };
+
+
+pub fn concat_mac<H: Digest>(hasher: &mut H, key: &[u8], message: &[u8]) -> Vec<u8> {
+    hasher.hash(&[key, message].concat())
+}
+
+pub fn sha1_mac(key: &[u8], message: &[u8]) -> Vec<u8> {
+    concat_mac(&mut Sha1::new(), key, message)
+}
+
+#[test]
+fn it_works() {
+    use rustc_serialize::hex::FromHex;
+
+    let key = b"SHA1";
+    let data = b"US Secure Hash Algorithm 1";
+
+    assert_eq!(
+        Sha1::new().hash(b""),
+        "da39a3ee5e6b4b0d3255bfef95601890afd80709".from_hex().unwrap()
+    );
+
+    assert_eq!(
+        Sha1::new().hash(data),
+        "fa1c3caec16f37de47773e7ad1e7e02f715e1960".from_hex().unwrap()
+    );
+
+    assert!(sha1_mac(key, data) == sha1_mac(key, data));
+    assert!(!(sha1_mac(key, data) == sha1_mac(key, key)));
+    assert!(!(sha1_mac(key, data) == sha1_mac(data, data)));
+}
---
+++ cryptopals/28-implement-a-sha-1-keyed-mac/src/sha1.rs
@@ -0,0 +1,142 @@
+use std::hash::Hasher;
+use std::io::{ self, Cursor, Write, BufWriter, BufReader };
+use byteorder::{ BigEndian, WriteBytesExt, ReadBytesExt };
+
+
+const H0: u32 = 0x67452301;
+const H1: u32 = 0xEFCDAB89;
+const H2: u32 = 0x98BADCFE;
+const H3: u32 = 0x10325476;
+const H4: u32 = 0xC3D2E1F0;
+
+
+#[derive(Clone, Debug)]
+pub struct Sha1(u32, u32, u32, u32, u32);
+
+impl Default for Sha1 {
+    fn default() -> Sha1 {
+        Sha1(H0, H1, H2, H3, H4)
+    }
+}
+
+impl Sha1 {
+    pub fn new() -> Sha1 {
+        Sha1::default()
+    }
+
+    fn process(&mut self, block: &[u8]) {
+        assert_eq!(block.len(), 64);
+
+        let mut words = [0; 80];
+        for (i, n) in block.chunks(4)
+            .map(|u| BufReader::new(u).read_u32::<BigEndian>().unwrap())
+            .enumerate()
+        {
+            words[i] = n;
+        }
+
+        for i in 16..80 {
+            words[i] = (
+                words[i-3]
+                    ^ words[i-8]
+                    ^ words[i-14]
+                    ^ words[i-16]
+            ).rotate_left(1);
+        }
+
+        let &mut Sha1(mut a, mut b, mut c, mut d, mut e) = self;
+        for (i, &n) in words.iter().enumerate() {
+            let (k, f) = match i {
+                0...19 => (0x5A827999, (b & c) | (!b & d)),
+                20...39 => (0x6ED9EBA1, b ^ c ^ d),
+                40...59 => (0x8F1BBCDC, (b & c) | (b & d) | (c & d)),
+                60...79 => (0xCA62C1D6, b ^ c ^ d),
+                _ => panic!("oop.")
+            };
+            let temp = a
+                .rotate_left(5)
+                .wrapping_add(f)
+                .wrapping_add(e)
+                .wrapping_add(k)
+                .wrapping_add(n);
+            e = d;
+            d = c;
+            c = b.rotate_left(30);
+            b = a;
+            a = temp;
+        }
+
+        self.0 = self.0.wrapping_add(a);
+        self.1 = self.1.wrapping_add(b);
+        self.2 = self.2.wrapping_add(c);
+        self.3 = self.3.wrapping_add(d);
+        self.4 = self.4.wrapping_add(e);
+    }
+
+    fn input(&mut self, data: &[u8]) {
+        let data = padding(data).unwrap();
+        for u in data.chunks(64) {
+            self.process(u);
+        }
+    }
+
+    fn output(&mut self) -> io::Result<Vec<u8>> {
+        let mut out = BufWriter::new(Vec::new());
+        out.write_u32::<BigEndian>(self.0)?;
+        out.write_u32::<BigEndian>(self.1)?;
+        out.write_u32::<BigEndian>(self.2)?;
+        out.write_u32::<BigEndian>(self.3)?;
+        out.write_u32::<BigEndian>(self.4)?;
+        Ok(out.into_inner()?)
+    }
+}
+
+pub trait Digest: Hasher {
+    fn digest(&mut self) -> Vec<u8>;
+    fn hash(&mut self, bytes: &[u8]) -> Vec<u8>;
+}
+
+impl Digest for Sha1 {
+    fn digest(&mut self) -> Vec<u8> {
+        self.output().unwrap()
+    }
+    fn hash(&mut self, bytes: &[u8]) -> Vec<u8> {
+        self.write(bytes);
+        self.digest()
+    }
+}
+
+impl Hasher for Sha1 {
+    fn write(&mut self, bytes: &[u8]) {
+        self.input(bytes)
+    }
+    fn finish(&self) -> u64 {
+        0
+    }
+}
+
+/// ```
+/// use implement_a_sha_1_keyed_mac::padding;
+/// let data = b"implement-a-sha-1-keyed-mac";
+/// let out = padding(data).unwrap();
+/// assert_eq!(
+///     out.len() % 64,
+///     0
+/// );
+/// ```
+pub fn padding(data: &[u8]) -> io::Result<Vec<u8>> {
+    let mut out = Cursor::new(Vec::new());
+    out.write(data)?;
+    out.write_u8(0x80)?;
+
+    if out.position() % (512 / 8) == (448 / 8) {
+        out.write_u8(0)?;
+    }
+
+    while out.position() % (512 / 8) != (448 / 8) {
+        out.write_u8(0)?;
+    }
+
+    out.write_u64::<BigEndian>(data.len() as u64 * 8)?;
+    Ok(out.into_inner())
+}
