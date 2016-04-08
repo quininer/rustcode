4988e7c64fc516e2f7d28434a54fbdc0b80c4469
quininer kel <quininer@live.com>
2016-04-08 12:53:10 +0800

    add(repo) implement-rsa

---
+++ cryptopals/39-implement-rsa/src/inv.rs
@@ -0,0 +1,52 @@
+use num::{ BigInt, Integer, Zero, One };
+
+
+lazy_static! {
+    pub static ref ZERO: BigInt = BigInt::zero();
+    pub static ref ONE: BigInt = BigInt::one();
+}
+
+pub fn invmod(e: &BigInt, et: &BigInt) -> Option<BigInt> {
+    let (gcd, c, _) = egcd(&e, &et);
+
+    if gcd != ONE.clone() {
+        None
+    } else if c >= ZERO.clone() {
+        Some(c)
+    } else {
+        Some(et + c)
+    }
+}
+
+pub fn egcd(a: &BigInt, b: &BigInt) -> (BigInt, BigInt, BigInt) {
+    let (mut r1, mut r2) = (a.clone(), b.clone());
+    let (mut s1, mut s2) = (ZERO.clone(), ONE.clone());
+    let (mut t1, mut t2) = (ONE.clone(), ZERO.clone());
+
+    while r2 != ZERO.clone() {
+        let (q, r3) = r1.div_mod_floor(&r2);
+        r1 = r2;
+        r2 = r3;
+
+        let s3 = &s1 - &q * &s2;
+        s1 = s2;
+        s2 = s3;
+
+        let t3 = &t1 - &q * &t2;
+        t1 = t2;
+        t2 = t3;
+    }
+
+    (r1, t1, s1)
+}
+
+#[test]
+fn test_invmod() {
+    assert_eq!(
+        invmod(
+            &BigInt::from(17),
+            &BigInt::from(3120)
+        ),
+        Some(BigInt::from(2753))
+    );
+}
---
+++ cryptopals/39-implement-rsa/src/lib.rs
@@ -0,0 +1,84 @@
+extern crate num;
+extern crate rustc_serialize;
+extern crate cbc_bitflipping_attacks;
+#[macro_use] extern crate lazy_static;
+#[macro_use] extern crate an_ebccbc_detection_oracle;
+#[macro_use] extern crate implement_diffie_hellman;
+
+mod inv;
+
+use num::BigUint;
+use num::bigint::{ ToBigInt, ToBigUint };
+use rustc_serialize::hex::FromHex;
+use cbc_bitflipping_attacks::Cipher;
+use implement_diffie_hellman::{ modexp, ONE as UONE };
+pub use inv::{ invmod, egcd, ZERO, ONE };
+
+
+lazy_static!{
+    pub static ref PRIMES: Vec<BigUint> = include_str!("primes.txt").lines()
+        .map(|u| u.from_hex().unwrap())
+        .map(|u| BigUint::from_bytes_be(&u))
+        .collect();
+}
+
+pub struct RSA {
+    sk: Option<BigUint>,
+    pk: BigUint,
+    n : BigUint
+}
+
+impl Default for RSA {
+    fn default() -> RSA {
+        let p = &rand!(choose PRIMES.clone());
+        let q = &rand!(choose PRIMES.clone());
+        let n = p * q;
+        let et = (p - UONE.clone()) * (q - UONE.clone());
+        let e = BigUint::from(3u32);
+
+        // FIXME BigInt or BigUint ?
+        let d = invmod(
+            &e.to_bigint().unwrap(),
+            &et.to_bigint().unwrap()
+        ).and_then(|u| u.to_biguint());
+
+        RSA::new(&d, &e, &n)
+    }
+}
+
+impl RSA {
+    pub fn new(sk: &Option<BigUint>, pk: &BigUint, n: &BigUint) -> RSA {
+        RSA { sk: sk.clone(), pk: pk.clone(), n: n.clone() }
+    }
+}
+
+impl Cipher for RSA {
+    fn encrypt(&self, data: &[u8]) -> Vec<u8> {
+        modexp(
+            BigUint::from_bytes_be(data),
+            self.pk.clone(),
+            self.n.clone()
+        ).to_bytes_be()
+    }
+    fn decrypt(&self, data: &[u8]) -> Vec<u8> {
+        modexp(
+            BigUint::from_bytes_be(data),
+            self.sk.clone().unwrap(),
+            self.n.clone()
+        ).to_bytes_be()
+    }
+}
+
+
+#[test]
+fn it_works() {
+    let plaintext = b"oh my rsa!";
+    let rsa = RSA::default();
+
+    let ciphertext = rsa.encrypt(plaintext);
+
+    assert_eq!(
+        rsa.decrypt(&ciphertext),
+        plaintext
+    );
+}
